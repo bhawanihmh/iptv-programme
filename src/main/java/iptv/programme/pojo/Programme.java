e1972a2fa7a4af2f14269e1f199705bc0e5e2e1d
BHAWANI SINGH SHEKHAWAT <bhawani.hmh@gmail.com>
2017-02-17 17:56:24 +0530

    Update Programme.java

--- src/main/java/iptv/programme/pojo/Programme.java
+++ src/main/java/iptv/programme/pojo/Programme.java
@@ -1,27 +1,18 @@
 package iptv.programme.pojo;
 
 import java.io.Serializable;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
 /**
  * 
  * @author bhawani.singh
  *
  */
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({ "progId", "progName", "progDuration" })
 public class Programme implements Serializable {
 
-	@JsonProperty("progId")
 	private String progId;
-	@JsonProperty("progName")
 	private String progName;
-	@JsonProperty("progDuration")
 	private Integer progDuration;
-	@JsonIgnore
+	
 	private final static long serialVersionUID = -277195824410767091L;
 
 	/**
@@ -44,32 +35,26 @@ public class Programme implements Serializable {
 		this.progDuration = progDuration;
 	}
 
-	@JsonProperty("progId")
 	public String getProgId() {
 		return progId;
 	}
 
-	@JsonProperty("progId")
 	public void setProgId(String progId) {
 		this.progId = progId;
 	}
 
-	@JsonProperty("progName")
 	public String getProgName() {
 		return progName;
 	}
 
-	@JsonProperty("progName")
 	public void setProgName(String progName) {
 		this.progName = progName;
 	}
 
-	@JsonProperty("progDuration")
 	public Integer getProgDuration() {
 		return progDuration;
 	}
 
-	@JsonProperty("progDuration")
 	public void setProgDuration(Integer progDuration) {
 		this.progDuration = progDuration;
 	}
